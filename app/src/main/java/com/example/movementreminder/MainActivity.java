ef1b0a536de052d92a3a860d14be521c9dda2d17
Marco-Cen <marco.c3n@gmail.com>
2021-05-26 13:07:26 -0400

    Initial Setup. Title Page.

---
+++ app/src/main/java/com/example/movementreminder/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.movementreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //-- Sets title of application --
+        setTitle("Welcome");
+//        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+}
