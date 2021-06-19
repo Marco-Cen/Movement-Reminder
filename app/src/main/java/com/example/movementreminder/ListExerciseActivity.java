0f66277cf76f8caf877a9e98593015956c03b506
thisseemslegit <rafaelaragon@live.ca>
2021-06-19 01:50:42 -0400

    added  grid layout
    added buttons to the selection exercise page

--- app/src/main/java/com/example/movementreminder/ListExerciseActivity.java
+++ app/src/main/java/com/example/movementreminder/ListExerciseActivity.java
@@ -10,5 +10,7 @@ public class ListExerciseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_exercise);
+
     }
+
 }
