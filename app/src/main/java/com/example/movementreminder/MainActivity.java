c0dd25726e48980772e0f2d4788d89f76a67e847
mirnafaltes <73962138+mirnafaltes@users.noreply.github.com>
2021-07-07 18:12:38 -0400

    pain form

--- app/src/main/java/com/example/movementreminder/MainActivity.java
+++ app/src/main/java/com/example/movementreminder/MainActivity.java
@@ -31,21 +31,10 @@ public class MainActivity extends AppCompatActivity {
            public void onClick(View v){
                //Functionality to open/redirect to (OTHER) PAGE
                openPainFormPage(); //Pain Form Page
-
-               Spinner dropdown = findViewById(R.id.spinner1);
-//create a list of items for the spinner.
-               String[] items = new String[]{"lower Back ", "Middle back ", "Upper Back "};
-//create an adapter to describe how the items are displayed, adapters are used in several places in android.
-//There are multiple variations of this, but this is the basic variant.
-               ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-//set the spinners adapter to the previously created one.
-               dropdown.setAdapter(adapter);
            }
 
 
         });
-
-
         //-- TEST: Linking Button to redirect to page
         Button bttnExerciseSelected;
         bttnExerciseSelected = findViewById((R.id.exerciseSelectedBttn));
--- app/src/main/java/com/example/movementreminder/PainFormActivity.java
+++ app/src/main/java/com/example/movementreminder/PainFormActivity.java
@@ -3,6 +3,8 @@ package com.example.movementreminder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class PainFormActivity extends AppCompatActivity {
 
@@ -14,15 +16,16 @@ public class PainFormActivity extends AppCompatActivity {
         //-- Sets title of application in Action Menu (Bar at very top) --
         setTitle("Pain Form");
 
+//get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner1);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"lower back ", "middle back ", "Upper back "};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
     }
 }
 
-    //get the spinner from the xml.
-    Spinner dropdown = findViewById(R.id.spinner1);
-    //create a list of items for the spinner.
-    String[] items = new String[]{"lower back ", "middle back ", "Upper back "};
-    //create an adapter to describe how the items are displayed, adapters are used in several places in android.
-//There are multiple variations of this, but this is the basic variant.
-    ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-//set the spinners adapter to the previously created one.
-dropdown.setAdapter(adapter);
+
